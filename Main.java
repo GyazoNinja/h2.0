36b8ea092ec0e4456e8ba0e2f2880ef2d9688507
Mike <mnicholas@albany.edu>
2017-02-26 19:43:11 -0500

    Created Main Credentials
    Created main string, integers necessary for the program as layed out by
    the directions(input, error, guess), and created a scanner/print
    statement instructing the user to put in their input and level of error.

---
+++ Main.java
@@ -0,0 +1,15 @@
+package squareRoot;
+import java.util.*;
+public class Main {
+public static void main(String []args){
+	double input, error, guess;
+
+	Scanner userInput = new Scanner(System.in);
+	System.out.println("In the console, put your input number followed by the level of error.");
+	input = userInput.nextDouble();
+	input = userInput.nextDouble();
+
+
+
+}
+}
