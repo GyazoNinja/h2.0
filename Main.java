ea7ebcc5220a2485a9f3c9e63d02a6b01cade699
Mike <mnicholas@albany.edu>
2017-02-26 20:24:45 -0500

    Completed Project
    Commented out code and added some print statements for clarity.

--- Main.java
+++ Main.java
@@ -3,19 +3,26 @@ import java.util.*;
 public class Main {
 
 public static void main(String []args){
+	// constructor for the babylonian method, created so that the print statement works
 	Recursion babylonian = new Recursion();
-	// create doubles to be used in babylonian method
+	// create doubles to be used in babylonian method, user input and user error
 	double input, error;
 
+	//scanners and following print statements
+	//tells the user how to input a number for the input and error
 	Scanner userInput = new Scanner(System.in);
 	System.out.print("In the console, put your input and hit enter.");
 	input = userInput.nextDouble();
 	System.out.print("In the console, put your error and hit enter.");
 	error = userInput.nextDouble();
-	
+	//once the user puts in their inputs, 
+	//the program calculates the guess based on the input and returns it
+	System.out.print("The Babylonian method guesses... ");
+	System.out.print(babylonian.method(input, error, input / 2));
+	System.out.print("!");
 	//
 	
-	System.out.print(babylonian.method(input, error, input / 2));
+
 
 
 }
--- Recursion.java
+++ Recursion.java
@@ -3,11 +3,19 @@ package squareRoot;
 public class Recursion {
 
 	public double method(double input, double error, double guess){
-	if ((guess * guess - input) <= error){
-		return guess;
+	//if statement to check if guess squared minus the input is less than the error.
+	//if it is less than the input, than the recursion is finished and the final guess is returned.	
+		if ((guess * guess - input) <= error){
+			return guess;
 	}
-	return method(input, error, .5 * (guess + input/guess)); 
-	}
+	//recursive method to make the guess more and more accurate
+	//input and error never change
+	//the new guess being used in each new recursion is equal to
+	// 0.5 *(last guess + X / last guess)
+	//keeps going until the if statement above is satistfied.
+		return method(input, error, .5 * (guess + input/guess)); 
+		}
 	
+	//constructor for the babylonian method to be able to be used in the print method in the main
 	public Recursion(){}
 }
